b0aa158d2b132aae32d06760c035144c879e160c
Piet Cordemans <piet.cordemans@kuleuven.be>
2016-10-05 16:35:36 +0200

    own router with home, about and 404

--- chapter3/hello.js
+++ chapter3/hello.js
@@ -3,10 +3,17 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch (path) {
+        case "": res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Home");
+            break;
+        case "/about": res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("About");
+            break;
+        default: res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("This is not the page you are looking for...");
+            break;  }});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
