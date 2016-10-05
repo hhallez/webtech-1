2e583fbc7f4ecd012bee08a1058e08e029cd0a10
Piet Cordemans <piet.cordemans@kuleuven.be>
2016-10-05 16:41:53 +0200

    serve static resources

--- chapter3/hello.js
+++ chapter3/hello.js
@@ -1,10 +1,23 @@
-const http = require('http');
+const http = require('http'), fs = require("fs");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+function serveStaticFile(res, path, contentType, responseCode) {
+  if(!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, (err,data) => {
+    if(err) {
+      res.writeHead(500, {"Content-Type": "text/plain"});
+      res.end("500 - Internal Error");
+    } else {
+      res.writeHead(responseCode, {"Content-Type": contentType});
+      res.end(data);
+    }
+  })
+}
+
 const server = http.createServer((req, res) => {
-    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
-    switch (path) {
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+  switch (path) {
         case "": res.writeHead(200, { "Content-Type": "text/plain" });
             res.end("Home");
             break;
@@ -13,8 +26,11 @@ const server = http.createServer((req, res) => {
             break;
         default: res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("This is not the page you are looking for...");
-            break;  }});
+            break;
+  }
+});
+
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
